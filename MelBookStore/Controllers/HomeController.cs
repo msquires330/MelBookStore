74a4f4f5505777a872c1be85713aa48152c25b37
Melissa Squires <mtuttle1@byu.edu>
2021-02-20 17:55:47 -0700

    Add project files.

---
+++ MelBookStore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MelBookStore.Models;
+
+namespace WaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private iStoreRepository _repository;
+
+        public HomeController(ILogger<HomeController> logger, iStoreRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.Projects);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MelBookStore/Models/SeedData.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MelBookStore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder application)
+        {
+            StoreDbContext context = application.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if(!context.Projects.Any())
+            {
+                context.Projects.AddRange(
+                    new Project
+                    {
+                        Title = "Les Miserables", 
+                        AuthorFirst = "Victor",
+                        AuthorLast = "Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419439", 
+                        Category = "Fiction, Classic", 
+                        Price = 9.95
+                    },
+
+                    new Project
+                    {
+                        Title = "Team of Rivals",
+                        AuthorFirst = "Doris",
+                        AuthorMiddle = "Kearns", 
+                        AuthorLast = "Goodwin",
+                        Publisher = "Simon & Schuster",
+                        ISBN = "978-0451419439",
+                        Category = "Fiction, Classic",
+                        Price = 9.95
+                    },
+
+                    new Project
+                    {
+                        Title = "The Snowball",
+                        AuthorFirst = "Alice",
+                        AuthorLast = "Schroeder",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553384611",
+                        Category = "Non-Fiction, Biography",
+                        Price = 21.54
+                    },
+
+                    new Project
+                    {
+                        Title = "American Ulysses",
+                        AuthorFirst = "Ronald",
+                        AuthorMiddle = "C.", 
+                        AuthorLast = "White",
+                        Publisher = "Random House",
+                        ISBN = "978-0812981254",
+                        Category = "Non-Fiction, Biography",
+                        Price = 11.61
+                    },
+
+                    new Project
+                    {
+                        Title = "Unbroken",
+                        AuthorFirst = "Laura",
+                        AuthorLast = "Hillenbrand",
+                        Publisher = "Random House",
+                        ISBN = "978-0812974492",
+                        Category = "Non-Fiction, Historical",
+                        Price = 13.33
+                    },
+
+                    new Project
+                    {
+                        Title = "The Great Train Robbery",
+                        AuthorFirst = "Michael",
+                        AuthorLast = "Crichton",
+                        Publisher = "Vintage",
+                        ISBN = "978-0804171281",
+                        Category = "Fiction, Historical Fiction",
+                        Price = 15.95
+                    },
+
+                    new Project
+                    {
+                        Title = "Deep Work",
+                        AuthorFirst = "Cal",
+                        AuthorLast = "Newport",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455586691",
+                        Category = "Non-Fiction, Self-Help",
+                        Price = 14.99
+                    },
+
+                    new Project
+                    {
+                        Title = "It's Your Ship",
+                        AuthorFirst = "Michael", 
+                        AuthorLast = "Abrashoff",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455523023",
+                        Category = "Non-Fiction, Self-Help",
+                        Price = 21.66
+                    },
+
+                    new Project
+                    {
+                        Title = "The Virgin Way",
+                        AuthorFirst = "Richard",
+                        AuthorLast = "Branson",
+                        Publisher = "Portfolio",
+                        ISBN = "978-1591847984",
+                        Category = "Non-Fiction, Business",
+                        Price = 29.16
+                    },
+
+                    new Project
+                    {
+                        Title = "Sycamore Row",
+                        AuthorFirst = "John",
+                        AuthorLast = "Grisham",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553393613",
+                        Category = "Fiction, Thrillers",
+                        Price = 15.03
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
